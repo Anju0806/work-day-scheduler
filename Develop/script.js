b1da637eeb777cff746370082cca67a8d9126639
anju <anjualfino@gmail.com>
2023-04-20 11:12:14 +0800

    changes in css

--- Develop/script.js
+++ Develop/script.js
@@ -12,24 +12,23 @@ $(function () {
   let current_hour = today.format('HH');
   
   // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  
   let t1;
   let newTable = $("<table>");
+ 
   let tableBody = $("<tbody>");
-  // create three rows with data
+ 
+  // create nine rows 
   for (let i = 9; i <= 17; i++) {
     let row1 = $("<tr>");
+    
     let cell1 = $("<td>");
+    cell1.addClass("row hour");
     if (i < 13) {
-      t1 = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(i + "AM");
+      t1 = $("<div>").addClass("col-2 col-md-1  text-center py-3 ").text(i + "AM");
     }
     else {
-      t1 = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(i - 12 + "PM");
+      t1 = $("<div>").addClass("col-2 col-md-1  text-center py-3 " ).text(i - 12 + "PM");
     }
     cell1.append(t1);
     row1.append(cell1);
@@ -75,26 +74,15 @@ $(function () {
   $("#myTableContainer").append(newTable);
 
 
-  
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-
-
-
   $(tableBody).on('click', '.saveBtn', function(event) {
-    // handle the click event on the save button here
+   
     var buttonId = $(this).attr('id');
     console.log('Save button clicked: ' + buttonId);
-  
-    // get the textarea associated with the save button
+    // get the textarea 
     var textareaId = buttonId.replace('button', 'textarea');
     var textareaValue = $('#' + textareaId).val();
-  
-    // save the textarea value to localStorage
+
     localStorage.setItem(textareaId, textareaValue);
-    //$(textareaId).text(localStorage.getItem(textareaValue));
   });
   
 });
